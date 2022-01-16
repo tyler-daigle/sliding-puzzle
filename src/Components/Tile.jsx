ef6cdec5770f9325d7a927889383baa30c653f58
Tyler Daigle <tyler@tdaigle.com>
2022-01-16 13:31:27 -0500

    tiles

--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -1,9 +1,9 @@
+import styles from "../style/Tile.module.css";
+
 export default function Tile({ tile, index }) {
   return (
-    <div>
-      <h1>
-        {tile.value} {index}
-      </h1>
+    <div className={styles.tile}>
+      <h1>{tile.value}</h1>
     </div>
   );
 }
